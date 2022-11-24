a8df65422a14a0975d40b94a9088342d1ae650fa
wendy <mizukilee@gmail.com>
2022-11-24 07:26:18 -0500

    commit tree 3c6270a5c3ad6140c5bfa6d8551da3c418b6cf61

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+
+
+
+
